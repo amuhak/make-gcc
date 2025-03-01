dbac089c4d9c42b75032afe4fc0ade0ad510653d
Amulya Jain <amuly.jain@gmail.com>
2025-03-01 15:07:56 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,84 +1,87 @@
 #!/bin/bash
-echo "This should be run in an empty folder"
+set -euo pipefail
 
+# Check if the current directory is empty (warn if not)
+if [ "$(ls -A)" ]; then
+    echo "Warning: This directory is not empty. It is recommended to run this script in an empty folder."
+fi
+
+# Update package lists and install required dependencies
+echo "Updating package lists..."
+sudo apt-get update
+
+echo "Installing required dependencies..."
+sudo apt install bzip2 flex build-essential -y
+
+# Determine the number of cores for parallel build
 no_of_cores=$(nproc --all)
+echo "You have ${no_of_cores} core(s)/thread(s) available."
 
-echo "You have ${no_of_cores} core/theads"
-
-echo "Cloning"
-
-git clone git://gcc.gnu.org/git/gcc.git
-
-read -p "Enter version number you want to build: " number
-
-# make sure that $number has 2 '.'
-period_count=$(echo "$number" | grep -o "\." | wc -l)
-
-if [ "$period_count" -eq 2 ]; then
-    echo "Version number is valid: $number"
+# Use the GCC repository if it exists; otherwise, clone it.
+if [ -d "gcc" ]; then
+    echo "GCC repository already exists. Updating the repository..."
+    cd gcc
+    git fetch --all
+    git reset --hard origin/master
+    cd ..
 else
-    echo "You input: $number dosn't have 2 '.' so it isnt a corect version"
-    echo "You must input something like 14.1.0"
-    echo "exiting"
-    exit 1
+    echo "Cloning GCC repository..."
+    git clone git://gcc.gnu.org/git/gcc.git
 fi
 
-rm build -rf
+# Prompt user for the GCC version to build
+read -p "Enter the GCC version you want to build (e.g., 14.1.0): " version
 
+# Validate that the version string has exactly two dots (format X.Y.Z)
+period_count=$(echo "$version" | grep -o "\." | wc -l)
+if [ "$period_count" -ne 2 ]; then
+    echo "Invalid version format: $version"
+    echo "You must input a version like 14.1.0 (i.e., X.Y.Z)"
+    echo "Exiting."
+    exit 1
+fi
+echo "Version number is valid: $version"
+
+# Prepare the build directory
+rm -rf build
+mkdir build
+
+# Enter the gcc source directory and check out the specified tag
 cd gcc
-
-echo "entering ./gcc and cleaning git"
-
-git clean -fd
-git reset --hard origin/master
-git fetch --all
-git reset --hard origin/master
-
-tagname="releases/gcc-${number}"
-
-echo "checking out $tagname"
-
-if ! git checkout "$tagname"; then
-    echo "Failed to check out tag $tagname. Please ensure it's a valid GCC version."
+echo "Checking out tag: releases/gcc-${version}"
+if ! git checkout "releases/gcc-${version}"; then
+    echo "Failed to check out tag releases/gcc-${version}. Please ensure it's a valid GCC version."
     exit 1
 fi
 
-echo "Make sure that that ^^^^^^ ran correctly, i.e. you have a real gcc version"
-
-# geting some prerequisites
-echo "geting some prerequisites"
-
-contrib/download_prerequisites
+echo "Downloading prerequisites..."
+./contrib/download_prerequisites
 
 cd ..
 
-echo "The version that will be built: $number"
+echo "The version that will be built: $version"
 
-echo "installing some more prerequisites"
-sudo apt install bzip2 flex build-essential
-
-mkdir build
+# Create and enter the build directory
 cd build
-
-echo "Configuring"
-
 current_dir=$(pwd)
+prefix_path="${current_dir}/../gcc-${version}"
 
-prefix_path="${current_dir}/../gcc-${number}"
+echo "Configuring GCC build..."
+../gcc/configure -v \
+    --build=x86_64-linux-gnu \
+    --host=x86_64-linux-gnu \
+    --target=x86_64-linux-gnu \
+    --prefix="${prefix_path}" \
+    --program-suffix="-${version}" \
+    --enable-checking=release \
+    --enable-languages=c,c++,objc,fortran,ada,go,d \
+    --disable-multilib
 
-../gcc/configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu "--prefix=${prefix_path}" "--program-suffix=-${number}" --enable-checking=release --enable-languages=c,c++,fortran,go --disable-multilib
-
-echo "Building"
-
+echo "Building GCC with ${no_of_cores} cores..."
 make -j "${no_of_cores}"
 
-echo "Removing temp files"
-
-echo "installing"
-
+echo "Installing GCC..."
 sudo make install-strip
 
-echo "You will find the binarys in ./gcc-${number}/bin"
-echo "Done 🎉🎉🎉"
-
-cd ..
+echo "GCC build and installation complete!"
+echo "You can find the installed GCC binaries in: ${prefix_path}/bin"
